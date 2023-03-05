f3e99dc5972fb4b80688e132f01eba8e637255b9
! Lacia <98511408+WASMilk@users.noreply.github.com>
2023-03-05 09:23:28 +0900

    Update 404.js

--- 404.js
+++ 404.js
@@ -6,3 +6,24 @@ setInterval(() => {
         location.href = "https://page.m-masatodayo.tk/"
     count--;
 }, 1000);
+
+function showCursor(e) {
+  const cursor = document.querySelector('.cursor');
+  cursor.style.top = e.clientY + 'px';
+  cursor.style.left = e.clientX + 'px';
+
+  const rainbowText = document.querySelector('.rainbow-text');
+  const letters = rainbowText.querySelectorAll('span');
+  letters.forEach(letter => {
+    const randomHue = Math.floor(Math.random() * 360);
+    letter.style.color = `hsl(${randomHue}, 100%, 50%)`;
+  });
+}
+
+function hideCursor(e) {
+  const rainbowText = document.querySelector('.rainbow-text');
+  const letters = rainbowText.querySelectorAll('span');
+  letters.forEach(letter => {
+    letter.style.color = 'black';
+  });
+}
